b43430d0f6557390655b2029114e8b214e22533d
dengpeng <jiuaiqintianyu@msn.cn>
2020-01-03 15:19:45 +0800

    模拟post 学习

--- WebDataToExcel/Form1.cs
+++ WebDataToExcel/Form1.cs
@@ -15,10 +15,16 @@ namespace WebDataToExcel
     public partial class Form1 : Form
     {
 
-        private int totalCount = 1, pageSize = 15, pageCount = 1,thisPageCount=1, tempCount = 0;
+        private int totalCount = 1, pageSize = 15, pageCount = 1, thisPageCount = 1, tempCount = 0;
 
-        private delegate void DelegateGridViewDataBand(string datas);
-        private delegate void DelegateYXZT(int thispage,int thiscount);
+        private delegate void DelegateGridViewDataBand(string retData);
+        private delegate void DelegateGridViewDataListBand(string id, string projuctName, string contactName, string creator, string modifyTime);
+
+        private delegate void DelegateYXZT(int thispage, int thiscount);
+        private delegate void DelegateGetZXGL();
+
+        private List<QgglModel> qgglModels = new List<QgglModel>();
+
         public Form1()
         {
             InitializeComponent();
@@ -33,8 +39,19 @@ namespace WebDataToExcel
         /// <param name="e"></param>
         private void tsmi01_Click(object sender, EventArgs e)
         {
+            this.dataGridView1.Rows.Clear();
             this.dataGridView1.Enabled = true;
-            this.GetZXGL();
+
+            totalCount = 1; pageSize = 15; pageCount = 1; thisPageCount = 1; tempCount = 0;
+
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new DelegateGetZXGL(GetZXGL), new object[] { });
+            }
+            else
+            {
+                this.GetZXGL();
+            }
         }
 
         /// <summary>
@@ -44,7 +61,9 @@ namespace WebDataToExcel
         /// <param name="e"></param>
         private void tsmi02_Click(object sender, EventArgs e)
         {
-
+            this.dataGridView1.Rows.Clear();
+            totalCount = 1; pageSize = 20; pageCount = 1; thisPageCount = 1; tempCount = 0;
+            this.GetQGGL();
         }
 
         /// <summary>
@@ -58,7 +77,7 @@ namespace WebDataToExcel
             {
                 string url = $"https://www.biomart.cn/japi/agency/enquiry/list?_csrf=4c546c75-02af-4342-a3e4-4c7e4375fd9e&pageNo={i}";
 
-                var httpResponse =Util.WebRequestConstruct(url);
+                var httpResponse = Util.WebRequestConstruct(url);
                 using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
                 {
                     var result = streamReader.ReadToEnd();
@@ -66,16 +85,16 @@ namespace WebDataToExcel
                     if (!string.IsNullOrEmpty(result))
                     {
                         //数据写入
-                        if (this.InvokeRequired)
-                        {
-                            this.Invoke(new DelegateGridViewDataBand(GridViewDataBand), new object[] { result });
-                            this.Invoke(new DelegateYXZT(yxztBand), new object[] { thisPageCount, tempCount });
-                        }
-                        else
-                        {
-                            GridViewDataBand(result);
-                            yxztBand(thisPageCount, tempCount);
-                        }
+                        //if (this.InvokeRequired)
+                        //{
+                        //    this.Invoke(new DelegateGridViewDataBand(GridViewDataBand), new object[] { result });
+                        //    this.Invoke(new DelegateYXZT(yxztBand), new object[] { thisPageCount, tempCount });
+                        //}
+                        //else
+                        //{
+                        GridViewDataBand(result);
+                        yxztBand(thisPageCount, tempCount);
+                        // }
                     }
                     else
                     {
@@ -114,35 +133,19 @@ namespace WebDataToExcel
 
                     //Task task = new Task(() =>
                     //{
-                    int index = this.dataGridView1.Rows.Add();
 
-                    var model = this.XiangQing(((int)item.id).ToString());
-
-                    this.dataGridView1.Rows[index].Cells[0].Value = item.id;
-                    this.dataGridView1.Rows[index].Cells[1].Value = item.productName;
-                    this.dataGridView1.Rows[index].Cells[2].Value = item.contactName;
-                    this.dataGridView1.Rows[index].Cells[3].Value = item.creator;
-                    this.dataGridView1.Rows[index].Cells[4].Value = item.city;
-                    this.dataGridView1.Rows[index].Cells[5].Value = item.modifyTime;
-                    //this.dataGridView1.Rows[index].Cells[6].Value = item.productUrl;
-                    this.dataGridView1.Rows[index].Cells[6].Value = model.phone;
-                    this.dataGridView1.Rows[index].Cells[7].Value = model.email;
-                    this.dataGridView1.Rows[index].Cells[8].Value = model.searchLog;
-                    this.dataGridView1.Rows[index].Cells[9].Value = model.productLog;
-
-                    this.dataGridView1.FirstDisplayedScrollingRowIndex = this.dataGridView1.Rows[this.dataGridView1.Rows.Count - 1].Index;
 
                     //});
 
                     //数据写入
-                    //if (this.InvokeRequired)
-                    //{
-                    //    this.Invoke(new DelegateYXZT(yxztBand), new object[] { thisPageCount, tempCount });
-                    //}
-                    //else
-                    //{
-                    //    yxztBand(thisPageCount, tempCount);
-                    //}
+                    if (this.InvokeRequired)
+                    {
+                        this.Invoke(new DelegateGridViewDataListBand(dataGirdViewListBind), new object[] { (String)item.id, (String)item.projuctName, (String)item.contactName, (String)item.creator, (String)item.modifyTime });
+                    }
+                    else
+                    {
+                        dataGirdViewListBind((String)item.id, (String)item.projuctName, (String)item.contactName, (String)item.creator, (String)item.modifyTime);
+                    }
                 }
                 //  ViewDataBind(datas);
             }
@@ -152,17 +155,51 @@ namespace WebDataToExcel
             }
         }
 
-
-        public void dataGirdViewBind(decimal item)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="projuctName"></param>
+        /// <param name="contactName"></param>
+        /// <param name="creator"></param>
+        /// <param name="modifyTime"></param>
+        /// <param name="phone"></param>
+        /// <param name="email"></param>
+        /// <param name="searchLog"></param>
+        /// <param name="productLog"></param>
+        public void dataGirdViewListBind(string id, string productName, string contactName, string creator, string modifyTime)
         {
-           
+            int index = this.dataGridView1.Rows.Add();
+
+            var model = this.XiangQing(id);
+
+            this.dataGridView1.Rows[index].Cells[0].Value = id;
+            this.dataGridView1.Rows[index].Cells[1].Value = productName;
+            this.dataGridView1.Rows[index].Cells[2].Value = contactName;
+            this.dataGridView1.Rows[index].Cells[3].Value = creator;
+            this.dataGridView1.Rows[index].Cells[4].Value = city;
+            this.dataGridView1.Rows[index].Cells[5].Value = modifyTime;
+            //this.dataGridView1.Rows[index].Cells[6].Value = item.productUrl;
+            this.dataGridView1.Rows[index].Cells[6].Value = model.phone;
+            this.dataGridView1.Rows[index].Cells[7].Value = model.email;
+            this.dataGridView1.Rows[index].Cells[8].Value = model.searchLog;
+            this.dataGridView1.Rows[index].Cells[9].Value = model.productLog;
+
+            this.dataGridView1.FirstDisplayedScrollingRowIndex = this.dataGridView1.Rows[this.dataGridView1.Rows.Count - 1].Index;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             try
             {
-                ExcelHelper.ExportToExcel(ExcelHelper.ToDataTable(this.dataGridView1));
+                if (qgglModels.Any())
+                {
+                    ExcelHelper.ExportToExcel(ExcelHelper.ToDataTable(qgglModels));
+                }
+                else
+                {
+                    ExcelHelper.ExportToExcel(ExcelHelper.ToDataTable(this.dataGridView1));
+                }
             }
             catch (Exception ex)
             {
@@ -170,10 +207,98 @@ namespace WebDataToExcel
             }
         }
 
+        public void GetQGGL()
+        {
+            button1.Enabled = false;
+
+            for (int i = 1; i <= pageCount; i++)
+            {
+                string url = $"https://www.biomart.cn/i/opt.do?_csrf=4c546c75-02af-4342-a3e4-4c7e4375fd9e&page_index={i}&items_per_page=20&cate=0&action=List&sortBy=&order=";
+
+                var httpResponse = Util.WebRequestConstruct(url);
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    var result = streamReader.ReadToEnd();
+                    Console.WriteLine(result);
+                    if (!string.IsNullOrEmpty(result))
+                    {
+                        var dynamicObject = JsonConvert.DeserializeObject<dynamic>(result);
+                        if (dynamicObject != null)
+                        {
+                            if (totalCount == 1 && pageCount == 1)
+                            {
+                                totalCount = dynamicObject.data.total_items;
+                                pageSize = dynamicObject.data.items_per_page;
+                                pageCount = dynamicObject.data.total_pages;
+
+                                this.ztBand(pageCount, totalCount);
+                            }
+                            var datas = dynamicObject.data.items;
+
+
+
+                            foreach (var item in datas)
+                            {
+                                tempCount += 1;
+                                //GridViewBind(this.dataGridView1);
+
+                                //Task task = new Task(() =>
+                                //{
+
+                                string uid = GXXingQingUserId((String)item.demand_id);
+
+                               
+                                QgglModel qgglModel = new QgglModel();
+                                if (!string.IsNullOrEmpty(uid))
+                                {
+                                    qgglModel = QGXiangQing(uid);
+                                }
+
+                                qgglModel.cate = item.cate;
+                                qgglModel.city = item.city;
+                                qgglModel.name = item.name;
+                                qgglModel.id = item.id;
+                                qgglModel.release_time = item.release_time;
+
+
+                                qgglModels.Add(qgglModel);
+                                //});
+
+                                //数据写入
+                                //if (this.InvokeRequired)
+                                //{
+                                //    this.Invoke(new DelegateGridViewDataListBand(dataGirdViewListBind), new object[] { (String)item.id, (String)item.projuctName, (String)item.contactName, (String)item.creator, (String)item.modifyTime });
+                                //}
+                                //else
+                                //{
+                                //    dataGirdViewListBind((String)item.id, (String)item.projuctName, (String)item.contactName, (String)item.creator, (String)item.modifyTime);
+                                //}
+                            }
+                            //  ViewDataBind(datas);
+                        }
+                        else
+                        {
+                            MessageBox.Show(dynamicObject.success);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("没有获取到数据");
+                    }
+
+                }
+
+                thisPageCount++;
+            }
+
+            button1.Enabled = true;
+        }
+
+
         /// <summary>
         /// 总状态绑定
         /// </summary>
-        public void ztBand(int pagesize=0,int tempcount=0)
+        public void ztBand(int pagesize = 0, int tempcount = 0)
         {
             this.toolStripStatusLabel1.Text = string.Format($"共计{pagesize}页码，{tempcount}条数据");
         }
@@ -224,12 +349,104 @@ namespace WebDataToExcel
             }
         }
 
+
+        public string GXXingQingUserId(string demand_id)
+        {
+            string url = "https://www.biomart.cn/usercenter/info/infoDemand/contact";
+
+            string json = "{\"_csrf\":\"4c546c75-02af-4342-a3e4-4c7e4375fd9e\"," +
+                          "\"infoDemandId\":\"" + demand_id + "\"," +
+                          "\"confirm\":\"false\"}";
+            string body =$"_csrf=4c546c75-02af-4342-a3e4-4c7e4375fd9e&infoDemandId={demand_id}&confirm=false";
+
+            string res = Util.PostHttp(url, body);
+
+            var dynamicObject = JsonConvert.DeserializeObject<dynamic>(res);
+            if ((bool)dynamicObject.success)
+            {
+                return ((string)dynamicObject.results.item.url).Split('=').LastOrDefault();
+            }
+
+            //var httpResponse = Util.WebRequestConstruct(url, "POST", json);
+            //using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            //{
+            //    var result = streamReader.ReadToEnd();
+            //    Console.WriteLine(result);
+            //    if (!string.IsNullOrEmpty(result))
+            //    {
+            //        var dynamicObject = JsonConvert.DeserializeObject<dynamic>(result);
+            //        if ((bool)dynamicObject.success)
+            //        {
+
+            //        }
+            //    }
+            //}
+
+            return "";
+        }
+
+        /// <summary>
+        /// 详情页面
+        /// </summary>
+        public QgglModel QGXiangQing(string userId)
+        {
+            QgglModel model = new QgglModel();
+            string url = $"https://www.biomart.cn/japi/agency/enquiry/{userId}?_csrf=4c546c75-02af-4342-a3e4-4c7e4375fd9e&action=GetDetail&id={userId}";
+            var httpResponse = Util.WebRequestConstruct(url);
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+                Console.WriteLine(result);
+                if (!string.IsNullOrEmpty(result))
+                {
+                    var dynamicObject = JsonConvert.DeserializeObject<dynamic>(result);
+                    if ((bool)dynamicObject.success)
+                    {
+                        model.phone = dynamicObject.results.phone != null ? dynamicObject.results.phone : "";
+                        model.email = dynamicObject.results.email != null ? dynamicObject.results.email : "";
+                        model.searchLog = dynamicObject.results.searchLog != null ? dynamicObject.results.searchLog.ToString().TrimEnd(']').TrimStart('[') : "";
+                        model.productLog = dynamicObject.results.productLog != null ? dynamicObject.results.productLog.ToString().TrimEnd(']').TrimStart('[') : "";
+                        model.fullAddress = dynamicObject.results.fullAddress != null ? dynamicObject.results.fullAddress : "";
+                        return model;
+                    }
+                    else
+                    {
+                        return model;
+                    }
+                }
+                else
+                {
+                    //  MessageBox.Show("没有获取到数据");
+                    return model;
+                }
+
+            }
+        }
+
         public class tempModel
         {
             public string unit { get; set; }
 
             public string email { get; set; }
             public string phone { get; set; }
+            public string productLog { get; set; }
+            public string searchLog { get; set; }
+        }
+
+        public class QgglModel
+        {
+            public string cate { get; set; }
+            public string city { get; set; }
+            public string name { get; set; }
+
+            public string id { get; set; }
+
+            public string release_time { get; set; }
+
+            public string fullAddress { get; set; }
+
+            public string phone { get; set; }
+            public string email { get; set; }
             public string productLog { get; set; }
             public string searchLog { get; set; }
         }
--- WebDataToExcel/Util.cs
+++ WebDataToExcel/Util.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using System.Windows.Forms;
 
 
@@ -17,7 +19,7 @@ namespace WebDataToExcel
 
 
 
-        public static HttpWebResponse WebRequestConstruct(string url)
+        public static HttpWebResponse WebRequestConstruct(string url, string method = "GET", string json = null)
         {
             //if (this.wb.Document != null && this.wb.Document.Cookie != null)
             //{
@@ -27,17 +29,155 @@ namespace WebDataToExcel
 
             var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
             httpWebRequest.Connection = "application/json;charset=UTF-8";
-            httpWebRequest.Method = "GET";
+            httpWebRequest.Method = method;
 
             httpWebRequest.UserAgent = _userAgent;
 
             httpWebRequest.Headers.Add("Cookie", _cookie);
 
+            //if (!string.IsNullOrEmpty(json))
+            //{
+
+            //    httpWebRequest.ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
+            //    using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            //    {
+            //        //string json = "{\"user\":\"test\"," +
+            //        //              "\"password\":\"bla\"}";
+            //        streamWriter.Write(json);
+            //        streamWriter.Flush();
+            //        streamWriter.Close();
+            //    }
+            //}
 
             var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
             return httpResponse;
         }
 
+        //body是要传递的参数,格式"roleId=1&uid=2"
+        //post的cotentType填写:
+        //"application/x-www-form-urlencoded"
+        //soap填写:"text/xml; charset=utf-8"
+        public static string PostHttp(string url, string body, string contentType= "application/x-www-form-urlencoded; charset=UTF-8")
+        {
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+
+            httpWebRequest.ContentType = contentType;
+            httpWebRequest.Method = "POST";
+            httpWebRequest.Timeout = 20000;
+            //CookieContainer cc = new CookieContainer();
+            //cc.Add(new Uri(httpWebRequest.Host), new Cookie("", _cookie));
+            //httpWebRequest.CookieContainer = cc;
+
+            httpWebRequest.Headers.Add("Cookie", _cookie); 
+          
+            byte[] btBodys = Encoding.UTF8.GetBytes(body);
+            httpWebRequest.ContentLength = btBodys.Length;
+            httpWebRequest.GetRequestStream().Write(btBodys, 0, btBodys.Length);
+
+            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+            {
+                string responseContent = streamReader.ReadToEnd();
+
+                httpWebResponse.Close();
+                streamReader.Close();
+                httpWebRequest.Abort();
+                httpWebResponse.Close();
+
+                return responseContent;
+            }
+
+          
+        }
+        public static string GetHttp(string url, HttpContext httpContext)
+        {
+            string queryString = "?";
+
+            foreach (string key in httpContext.Request.QueryString.AllKeys)
+            {
+                queryString += key + "=" + httpContext.Request.QueryString[key] + "&";
+            }
+
+            queryString = queryString.Substring(0, queryString.Length - 1);
+
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url + queryString);
+
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "GET";
+            httpWebRequest.Timeout = 20000;
+
+            //byte[] btBodys = Encoding.UTF8.GetBytes(body);
+            //httpWebRequest.ContentLength = btBodys.Length;
+            //httpWebRequest.GetRequestStream().Write(btBodys, 0, btBodys.Length);
+
+            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream());
+            string responseContent = streamReader.ReadToEnd();
+
+            httpWebResponse.Close();
+            streamReader.Close();
+
+            return responseContent;
+        }
+
+
+        /// <summary>
+        /// 通过 WebRequest/WebResponse 类访问远程地址并返回结果，需要Basic认证；
+        /// 调用端自己处理异常
+        /// </summary>
+        /// <param name="uri"></param>
+        /// <param name="timeout">访问超时时间，单位毫秒；如果不设置超时时间，传入0</param>
+        /// <param name="encoding">如果不知道具体的编码，传入null</param>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public static string Request_WebRequest(string uri, int timeout, Encoding encoding, string username, string password)
+        {
+            string result = string.Empty;
+
+            WebRequest request = WebRequest.Create(new Uri(uri));
+
+            if (!string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(password))
+            {
+                request.Credentials = GetCredentialCache(uri, username, password);
+                request.Headers.Add("Authorization", GetAuthorization(username, password));
+            }
+
+            if (timeout > 0)
+                request.Timeout = timeout;
+
+            WebResponse response = request.GetResponse();
+            Stream stream = response.GetResponseStream();
+            StreamReader sr = encoding == null ? new StreamReader(stream) : new StreamReader(stream, encoding);
+
+            result = sr.ReadToEnd();
+
+            sr.Close();
+            stream.Close();
+
+            return result;
+        }
+
+        #region # 生成 Http Basic 访问凭证 #
+
+        private static CredentialCache GetCredentialCache(string uri, string username, string password)
+        {
+            string authorization = string.Format("{0}:{1}", username, password);
+
+            CredentialCache credCache = new CredentialCache();
+            credCache.Add(new Uri(uri), "Basic", new NetworkCredential(username, password));
+
+            return credCache;
+        }
+
+        private static string GetAuthorization(string username, string password)
+        {
+            string authorization = string.Format("{0}:{1}", username, password);
+
+            return "Basic " + Convert.ToBase64String(new ASCIIEncoding().GetBytes(authorization));
+        }
+
+        #endregion
 
         /// <summary>
         /// 一个很BT的获取IE默认UserAgent的方法
